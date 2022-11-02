3795699dbcae87f0198a7cb7e96269b939975fd5
thx4u <48453875+thxForu@users.noreply.github.com>
2022-11-02 15:03:12 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Api;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $response = Telegram::getMe();
+    $botId = $response->getId();
+    $firstName = $response->getFirstName();
+    $username = $response->getUsername();
+    echo $botId . '<br />' . $firstName . '<br />' . $username;
+
+    //return view('welcome');
 });
